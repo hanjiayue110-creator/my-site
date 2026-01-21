176f72060e1b997960c17220bc6fc8c30c72091e
韩佳跃 <hjy123457@Mac.local>
2026-01-21 18:38:25 +0800

    update layout

--- main.js
+++ main.js
@@ -1,18 +1,10 @@
-{\rtf1\ansi\ansicpg936\cocoartf2822
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+const btn = document.getElementById("btn");
+const title = document.getElementById("title");
+const year = document.getElementById("year");
 
-\f0\fs24 \cf0 const btn = document.getElementById("btn");\
-const title = document.getElementById("title");\
-const year = document.getElementById("year");\
-\
-year.textContent = new Date().getFullYear();\
-\
-btn.addEventListener("click", () => \{\
-  const now = new Date().toLocaleString();\
-  title.textContent = "\uc0\u20320 \u28857 \u21040 \u20102 \u65281 \u26102 \u38388 \u65306 " + now;\
-\});\
-}
+year.textContent = new Date().getFullYear();
+
+btn.addEventListener("click", () => {
+  const now = new Date().toLocaleString();
+  title.textContent = "你点击了按钮：" + now;
+});
